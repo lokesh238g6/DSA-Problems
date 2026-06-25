c5531fcfbf865ffe8e3e446a127df00361a6c975
Lokesh <153075022+lokesh238g6@users.noreply.github.com>
2026-06-25 20:51:48 +0530

    Time: 3 ms (96.11%), Space: 46.6 MB (49.22%) - LeetHub

---
+++ 0006-zigzag-conversion/0006-zigzag-conversion.java
@@ -0,0 +1,28 @@
+class Solution 
+{
+    public String convert(String s, int numRows) 
+    {
+        if (numRows==1 || numRows>=s.length()) 
+        {
+            return s;
+        }
+        StringBuilder ans=new StringBuilder();
+        int cycle=2*numRows-2;
+
+        for (int row = 0; row<numRows; row++) 
+        {
+            for (int j=row; j<s.length();j+=cycle) {
+
+                ans.append(s.charAt(j));
+
+                int diagonal=j+cycle-2*row;
+
+                if (row!=0 && row!=numRows-1 && diagonal<s.length())
+                {
+                    ans.append(s.charAt(diagonal));
+                }
+            }
+        }
+        return ans.toString();
+    }
+}
